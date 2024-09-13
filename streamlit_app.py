ba4152c7092c434d62537d19b39f037240bc2a9f
wlastraquillo <wlastraquillo@yahoo.com>
2024-09-13 19:10:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,6 +54,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,'+" "+name_on_order, icon="✅")
 
+    # New section to display fruityvice nutrition information
     import requests
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    st.text(fruityvice_response)
+    #st.text(fruityvice_response.json())
+    fv_df = st.dataframe(data=fruityvice_response.json(), use_contrainer_width=True)
