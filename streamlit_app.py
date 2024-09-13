71aff0ed7a5488497abb82cf085ebe8b7808c21f
wlastraquillo <wlastraquillo@yahoo.com>
2024-09-13 19:38:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
-import requests
 # Import python packages
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col
+from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
 st.title("Customized Your Smoothie! :cup_with_straw:")
@@ -19,11 +17,7 @@ st.write("The name on your Smoothie will be: ", name_on_order)
 
 from snowflake.snowpark.functions import col
 
-#session = get_active_session()
-
-cnx = st.connection("snowflake")
-session = cnx.session()
-
+session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect(
@@ -40,10 +34,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_contrainer_width=True)
-        
+
     #st.write(ingredient_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -57,5 +48,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,'+" "+name_on_order, icon="✅")
-
-  
